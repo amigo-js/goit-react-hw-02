c849b4bd007179338d4a57673b17a1acc20deb2a
vito <working.space.js@gmail.com>
2024-09-08 18:22:02 +0200

    Add correction based on mentor comment

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,7 +4,7 @@ import Options from '../Options/Options';
 import Description from '../Description/Description';
 import Notification from '../Notification/Notification';
 
-import style from "./App.module.css";
+import style from './App.module.css';
 
 export default function App() {
   const [feedback, setFeedback] = useState(() => {
@@ -19,10 +19,8 @@ export default function App() {
     window.localStorage.setItem('feedback', JSON.stringify(feedback));
   }, [feedback]);
 
-  
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-  
   const updateFeedback = feedbackType => {
     setFeedback(prevFeedback => ({
       ...prevFeedback,
@@ -30,10 +28,12 @@ export default function App() {
     }));
   };
 
-
   const resetFeedback = () => {
     setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
+
+  const positiveFeedback =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
   return (
     <div className={style.container}>
@@ -44,7 +44,7 @@ export default function App() {
         hasFeedback={totalFeedback > 0}
       />
       {totalFeedback > 0 ? (
-        <Feedback feedback={feedback} />
+        <Feedback feedback={feedback} positiveFeedback={positiveFeedback} />
       ) : (
         <Notification message="No feedback given yet." />
       )}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,10 +1,9 @@
-export default function Feedback({ feedback }) {
-  if (!feedback) return <p>No feedback available.</p>; // Проверка на наличие feedback
+export default function Feedback({ feedback, positiveFeedback }) {
+  if (!feedback) return <p>No feedback available.</p>;
 
   const { good, neutral, bad } = feedback;
   const totalFeedback = good + neutral + bad;
-  const positiveFeedback =
-    totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+  
 
   return (
     <div>
